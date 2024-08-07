852c58839f6473d788cb2f374cfca40e5bdd71b0
Nitin Singh <nitintwt27@gmail.com>
2024-08-07 20:07:19 +0530

    fixing bugs for aws deployment

--- user-service/src/Client.js
+++ user-service/src/Client.js
@@ -1,6 +1,11 @@
-import {Redis} from "ioredis"
+import {createClient} from 'redis'
 
 // using this client we can interact with our redis server
-const client = new Redis()
+const client = createClient({
+  socket:{
+    host:process.env.REDIS_HOST,
+    port:process.env.REDIS_PORT
+  }
+})
 
 export default client
